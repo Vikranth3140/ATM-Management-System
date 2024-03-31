af8f56d7ccfe44bfe0e48fa51625f01e9162eaec
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-03-31 18:25:26 +0530

    Update .gitignore and database.py with personal MySQL credentials comment

--- db/database.py
+++ db/database.py
@@ -1,6 +1,7 @@
 import mysql.connector
 
 def get_db_connection():
+    # Change the user and password to your MySQL user and password
     connection = mysql.connector.connect(
         host="localhost",
         user="your_username",
